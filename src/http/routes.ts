ef7ef16e5d39a9ae678acfcb0d35e33e055e6e62
biluadev <biluadevcod@gmail.com>
2024-07-24 19:36:13 +0100

    Adicionado o test de autenticação!

--- src/http/routes.ts
+++ src/http/routes.ts
@@ -1,6 +1,10 @@
 import { FastifyInstance } from "fastify";
-import { register } from "./contollers/register.controller";
+import { register } from "./controllers/register.controller";
+import { authenticate } from "./controllers/authenticate.controller";
+
 
 export async function appRoutes(app: FastifyInstance) {
     app.post('/users', register)
+
+    app.post('/sessions', authenticate)
 }
